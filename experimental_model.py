a34e0309e394c5d0e8974cbfa9c613c968b17374
texsmv <texs.mv@gmail.com>
2018-07-18 04:03:20 -0500

    modelo multi input y multi output

---
+++ experimental_model.py
@@ -0,0 +1,80 @@
+from keras.layers import Input, Dense, LSTM, concatenate, CuDNNLSTM
+from keras.models import Model
+from sklearn.externals import joblib
+import numpy as np
+
+dt = joblib.load("dts.pkl")
+t = joblib.load("ts.pkl")
+p = joblib.load("ps.pkl")
+
+
+n = len(dt)
+
+i_dt = dt[0 : n - 1]
+o_dt = dt[1 : n]
+
+i_t = t[0 : n - 1]
+o_t = t[1 : n]
+
+i_p = p[0 : n - 1]
+o_p = p[1 : n]
+
+n = n - 1
+
+o_dt = o_dt.reshape(n , 1)
+o_t = o_t.reshape(n, 1)
+o_p = o_p.reshape(n, 101)
+
+n_classes = 101
+
+
+# i_dt = np.ones(shape = (n, 1, 1))
+# o_dt = np.ones(shape = (n, 1))
+#
+# i_t = np.ones(shape = (n, 1, 1))
+# o_t = np.ones(shape = (n, 1))
+#
+# i_p = np.ones(shape = (n, 1, n_classes))
+# o_p = np.ones(shape = (n, n_classes))
+
+
+input_dt = Input(shape = (1, 1), name = "input_dt")
+lstm_dt = CuDNNLSTM(128, return_sequences = True)(input_dt)
+
+input_t = Input(shape = (1, 1), name = "input_t")
+lstm_t = CuDNNLSTM(128, return_sequences = True)(input_t)
+
+input_p = Input(shape = (1, n_classes), name = "input_p")
+lstm_p = CuDNNLSTM(128, return_sequences = True)(input_p)
+
+
+hold_input = concatenate([lstm_dt, lstm_t, lstm_p])
+
+main_lstm = CuDNNLSTM(256, return_sequences = True)(hold_input)
+
+lstm_dt_o = CuDNNLSTM(128)(main_lstm)
+dt_output = Dense(1, activation = "relu", name = "dt_output")(lstm_dt_o)
+
+lstm_t_o = CuDNNLSTM(128)(main_lstm)
+t_output = Dense(1, activation = "relu", name = "t_output")(lstm_t_o)
+
+lstm_p_o = CuDNNLSTM(128)(main_lstm)
+p_output = Dense(n_classes, activation = "sigmoid", name = "p_output")(lstm_p_o)
+
+
+model = Model(inputs=[input_dt, input_t, input_p], outputs=[dt_output, t_output, p_output])
+
+model.compile(optimizer='rmsprop',
+              loss={'dt_output': 'mean_squared_error', 't_output': 'mean_squared_error', 'p_output': 'binary_crossentropy'})
+
+model.fit({'input_dt': i_dt, 'input_t': i_t, 'input_p': i_p},
+          {'dt_output': o_dt, 't_output': o_t, 'p_output': o_p},
+
+          epochs=15, batch_size=128)
+
+model.save("modelo_experimental.h5")
+# metrics = {'dt_output': ['mse', 'accuracy'], 't_output': ['mse', 'accuracy'], 'p_output': ['mse', 'accuracy']},
+a, b, c = model.predict([i_dt[:3], i_t[:3], i_p[:3]])
+print("p: ", c)
+print("dt: ",a)
+print("t: ",b)
---
+++ lectura.py
@@ -0,0 +1,104 @@
+import pretty_midi as pm
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa.display
+import os
+from sklearn.externals import joblib
+from sklearn.preprocessing import MinMaxScaler
+
+
+def int2bin(n, maximo):
+    # print(maximo, n)
+    temp = [0] * int(maximo)
+    temp[int(n - 1)] = 1
+    return temp
+
+
+
+duracionSet = [0.0625, 0.125, 0.25, 0.5,1, 2,4]
+
+def aproximar(d):
+    aprox = 0
+    dist = abs(d - duracionSet[0])
+    for i in range(1, len(duracionSet)):
+        if abs(duracionSet[i] - d ) < dist:
+            dist = abs(duracionSet[i] - d )
+            aprox = i
+    return aprox
+
+
+
+def read_midis(path, instrument = 0):
+    midis_p = os.listdir(path)
+    # midis, tempos[0] = get_vector(path + "/" + midis_p[0], instrument)
+    tempos = []
+    dts = []
+    ts = []
+    ps = []
+
+    for i in range(0, len(midis_p)):
+        mid, T = get_vector(path + "/" + midis_p[i], instrument)
+        for j in range(0, len(mid)):
+            dts.append(mid[j][0])
+            ts.append(mid[j][1])
+            ps.append(mid[j][2])
+        tempos.append(T)
+
+    return (dts, ts, ps), tempos
+
+
+def get_vector(file,n_instrument=0):
+    midi = pm.PrettyMIDI(file)
+    tempo = float(midi.estimate_tempo()/60)
+    print('tempo ',midi.estimate_tempo()) ,
+    print('instrumentos' , len(midi.instruments))
+
+    nNotas = len(midi.instruments[n_instrument].notes)
+    notas = np.zeros((nNotas,3),dtype='float64')
+
+    notaAnterior = midi.instruments[n_instrument].notes[0]
+
+
+    for i in range(0,nNotas):
+        notaActual = midi.instruments[n_instrument].notes[i]
+        dT = abs(notaActual.start - notaAnterior.start)
+        T = abs(notaActual.end - notaActual.start )
+        P = notaActual.pitch
+        notas[i][0] = dT
+        notas[i][1] = aproximar(T/tempo)
+        notas[i][2] = P
+
+        notaAnterior = notaActual
+
+    return notas, tempo
+
+
+
+(dts, ts, ps), tempos = read_midis("chopin",0)
+
+ps = np.array(ps)
+n_classes = np.max(ps.flatten())
+ps = [int2bin(e, n_classes) for e in ps]
+
+scaler =MinMaxScaler(feature_range=(0,10))
+# data = scaler.fit_transform(data)
+
+dts = np.array(dts)
+dts = dts.reshape(len(dts), 1)
+dts = scaler.fit_transform(dts)
+dts = dts.reshape(len(dts), 1, 1)
+
+ts = np.array(ts)
+ts = ts.reshape(len(ts), 1)
+ts = scaler.fit_transform(ts)
+ts = ts.reshape(len(ts), 1, 1)
+
+ps = np.array(ps)
+ps = ps.reshape(len(ts), 1, 101)
+tempos = np.array(tempos)
+
+
+joblib.dump(dts,"dts.pkl")
+joblib.dump(ts,"ts.pkl")
+joblib.dump(ps,"ps.pkl")
+joblib.dump(tempos,"tempos.pkl")
